8f3c71b2a0f6edc9f86c7f8a8eadbff3324961a6
Alria <artur.kirovich@gmail.com>
2022-12-25 18:45:44 +0600

    finished cleaning functions

--- js/app.js
+++ js/app.js
@@ -1,31 +1,44 @@
 "use strict";
-// fetch("/data.json")
-//   .then((response) => response.json())
-//   .then((json) => console.log(json));
 
 let fillerBox = document.querySelector(".input__filler-inner"),
   filterBtn = document.querySelectorAll(".job-list__filters-item"),
-  clearBtn = document.querySelector(".clear-btn");
+  fillerItem,
+  fillerArray = [];
 
 filterBtn.forEach((e) => {
   e.addEventListener("click", (i) => {
-    let fillerArray = [...fillerBox.childNodes];
-    let fillerItem = document.createElement("div");
+    fillerItem = document.createElement("div");
     fillerItem.className = "input__filler-item";
     fillerItem.innerHTML = `${i.target.innerHTML}`;
-    fillerBox.append(fillerItem);
-    fillerItem.insertAdjacentHTML(
-      "beforeend",
-      `<img
+    if (fillerArray.includes(fillerItem.textContent)) {
+      return;
+    } else {
+      fillerArray.push(i.target.textContent);
+      fillerBox.append(fillerItem);
+      fillerItem.insertAdjacentHTML(
+        "beforeend",
+        `<img
       class="cross-img"
       src="./images/icon-remove.svg"
-      alt=""
+      alt="cross-img"
     />`
-    );
-    fillerArray.filter((word) => {
-      if (word.textContent == i.target.innerHTML) {
-        fillerItem.remove();
+      );
+    }
+  });
+});
+
+document.querySelector("body").addEventListener("click", (i) => {
+  if (i.target.classList.contains("cross-img")) {
+    let closeItem = i.target.closest(".input__filler-item");
+    fillerArray.forEach((v, i) => {
+      if (closeItem.textContent == v) {
+        fillerArray.splice(i, 1);
+        closeItem.remove();
       }
     });
-  });
+  }
+  if (i.target.classList.contains("clear-btn")) {
+    fillerArray.splice(0, fillerArray.length);
+    document.querySelectorAll(".input__filler-item").forEach((e) => e.remove());
+  }
 });
